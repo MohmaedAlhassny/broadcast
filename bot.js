e3c6a6d99df27b0cce419887e189993dde7e5d7c
Mohmaed Ayad <40663543+MohmaedAlhassny@users.noreply.github.com>
2018-07-26 14:16:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ client.on('message', message => {
 	.setDescription(`تم أرسال رسالتك بنجاح.`)
 	.setColor('GREEN')
 	.setAuthor(message.author.tag, message.author.avatarURL)
-if(message.content.startsWith(prefix + 'bc')) {
+if(message.content === prefix + 'bc') {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
